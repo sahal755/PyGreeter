309455236d9a4ff456d3e2ed4dbddaa53794e67b
sahal755 <sahal2008sainudeen@gmail.com>
2026-08-16 20:44:53 +0530

    First Commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# Simple Python Program to Greet the User
+
+def greet_user():
+    name = input("Please enter your name: ")
+    print(f"Hello, {name}! Welcome to our program.")
+
+
+# Call the function
+greet_user()
